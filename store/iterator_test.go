34232a9fe5a3e278acdb3eab0e3970029437ad2d
Stéphane Duchesneau <stephane.duchesneau@eoscanada.com>
2020-06-02 11:41:58 -0400

    add iterator tests

---
+++ store/iterator_test.go
@@ -0,0 +1,48 @@
+package store
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIteratorPushStuck(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	it := NewIterator(ctx)
+	kv := &KV{}
+
+	// fill buffer
+	for i := 0; i < 100; i++ {
+		it.PushItem(kv)
+	}
+
+	cancel()
+	ok := it.PushItem(kv)
+	assert.False(t, ok)
+}
+
+func TestRaceFinished(t *testing.T) {
+	ctx := context.Background()
+
+	for i := 0; i < 100; i++ {
+		it := NewIterator(ctx)
+		kv := &KV{}
+		require.True(t, it.PushItem(kv))
+		it.PushFinished()
+		assert.True(t, it.Next())
+		assert.Equal(t, kv, it.Item())
+
+	}
+}
+
+func TestErrorExit(t *testing.T) {
+	ctx := context.Background()
+	it := NewIterator(ctx)
+	deadErr := errors.New("dead")
+	it.PushError(deadErr)
+	assert.False(t, it.Next())
+	assert.Equal(t, deadErr, it.Err())
+}
