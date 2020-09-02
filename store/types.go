641c10eec77bbaa5059b5790a758962b3c1b8527
Matthieu Vachon <matt@dfuse.io>
2020-09-02 15:58:41 -0400

    Added `Next` and `PrefixNext` to `store.Key` to easily compute the next and prefix next key

--- store/types.go
+++ store/types.go
@@ -17,6 +17,46 @@ func (k Key) String() string {
 	return hex.EncodeToString(k)
 }
 
+// Next returns the next key in byte-order.
+//
+// Copied from https://github.com/tikv/client-go/blob/master/key/key.go
+func (k Key) Next() Key {
+	// add 0x0 to the end of key
+	buf := make([]byte, len([]byte(k))+1)
+	copy(buf, []byte(k))
+	return buf
+}
+
+// PrefixNext returns the next prefix key.
+//
+// Assume there are keys like:
+//
+//   rowkey1
+//   rowkey1_column1
+//   rowkey1_column2
+//   rowKey2
+//
+// If we seek 'rowkey1' Next, we will get 'rowkey1_column1'.
+// If we seek 'rowkey1' PrefixNext, we will get 'rowkey2'.
+//
+// Copied from https://github.com/tikv/client-go/blob/master/key/key.go
+func (k Key) PrefixNext() Key {
+	buf := make([]byte, len([]byte(k)))
+	copy(buf, []byte(k))
+	var i int
+	for i = len(k) - 1; i >= 0; i-- {
+		buf[i]++
+		if buf[i] != 0 {
+			break
+		}
+	}
+	if i == -1 {
+		copy(buf, k)
+		buf = append(buf, 0)
+	}
+	return buf
+}
+
 type Limit int
 
 func (l Limit) Reached(count uint64) bool {
